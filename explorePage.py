18d4bc919e01b4194e13eff8819005b501e782b8
Akash Kumar <111282871+akash6171@users.noreply.github.com>
2025-06-10 17:15:19 +0530

    explorePage.py

--- explorePage.py
+++ explorePage.py
@@ -1,6 +1,6 @@
-import streamlit as st 
-import pandas as pd
-import matplotlib.pyplot as plt
+import streamlit as st # type: ignore
+import pandas as pd # type: ignore
+import matplotlib.pyplot as plt # type: ignore
 
 def shorten_categories(categories, cutoff):
     categorical_map = {}
@@ -8,7 +8,7 @@ def shorten_categories(categories, cutoff):
         if categories.values[i] >= cutoff:
             categorical_map[categories.index[i]] = categories.index[i]
         else:
-            categorical_map[categories.index[i]]='Other'
+            categorical_map[categories.index[i]] = 'Other'
     return categorical_map
 
 def clean_experience(x):
@@ -19,65 +19,63 @@ def clean_experience(x):
     return float(x)
 
 def clean_education(x):
-    if "Bachelor’s degree" in x:
-        return "Bachelor’s degree"
-    if "Master’s degree" in x:
-        return "Master’s degree"
+    if 'Bachelor’s degree' in x:
+        return 'Bachelor’s degree'
+    if 'Master’s degree' in x:
+        return 'Master’s degree'
     if 'Professional degree' in x or 'Other doctoral' in x:
         return 'Post grad'
     return 'Less than a Bachelors'
 
-@st.cache
+@st.cache_data
 def load_data():
-    survey = pd.read_csv('survey_results_public.csv')
-    survey = survey[["Country", "EdLevel", "YearsCodePro", "Employment", "ConvertedComp"]]
-    survey = survey.rename({"ConvertedComp": "Salary"}, axis=1)
-    survey = survey[survey['Salary'].notnull()]
-    survey = survey.dropna()
-    survey = survey[survey["Employment"] == "Employed full-time"]
-    survey = survey.drop("Employment", axis=1)
-    
-    country_map = shorten_categories(survey.Country.value_counts(), 400)
-    survey['Country'] = survey['Country'].map(country_map)
-    survey = survey[survey["Salary"] <= 250000]
-    survey = survey[survey["Salary"] >= 10000]
-    survey = survey[survey['Country'] != 'Other']
-    
-    survey['YearsCodePro'] = survey['YearsCodePro'].apply(clean_experience)
-    survey['EdLevel'] = survey['EdLevel'].apply(clean_education)
-    return survey
+    df = pd.read_csv('survey_results_public.csv')
+    df = df[['Country', 'EdLevel', 'YearsCodePro', 'Employment', 'ConvertedComp']]
+    df = df[df['ConvertedComp'].notnull()]
+    df = df.dropna()
+    df = df[df["Employment"] == "Employed full-time"]
+    df = df.drop("Employment", axis=1)
 
-survey = load_data()
 
-def showExplorePage():
-    st.title('Explore Software Engineer salaries')
-    st.write(
-    """
-    ### Stack Overflow Developer Survey 2020
-    """
-    )
-    
-    data = survey['Country'].value_counts()
-    fig1, ax1 = plt.subplots()
-    ax1.pie(data, labels=data.index, autopct="%1.1f%%", startangle=90)
-    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    
-    st.write("""### Number of data from different countries""")
-    st.pyplot(fig1)
-    
-    st.write(
-        """"
-        #### Mean Salary Based on Country
-        """
-    )
-    data = survey.groupby(['Country'])['Salary'].mean().sort_values(ascending=True)
-    st.bar_chart(data)
-    
+    country_map = shorten_categories(df.Country.value_counts(), 400)
+    df['Country'] = df['Country'].map(country_map) 
+    df = df[df["ConvertedComp"] <= 250000]
+    df = df[df["ConvertedComp"] >=10000]
+    df = df[df['Country'] != "Other"]
+
+    df['YearsCodePro'] = df['YearsCodePro'].apply(clean_experience)
+    df['EdLevel'] = df['EdLevel'].apply(clean_education)
+    df = df.rename({"ConvertedComp": "Salary"}, axis=1)
+    return df
+df = load_data()
+
+def show_explore_page():
+    st.title("Explore Software Engineer Salaries")
+
     st.write("""
-             #### Mean Salary Based on Experience
+             ###Stack Overflow Developer Survey 2020
              """)
-    
-    data = survey.groupby(['YearsCodePro'])['Salary'].mean().sort_values(ascending=True)
-    st.line_chart(data)
+    data = df["Country"].value_counts()
 
+    fig1, ax1 = plt.subplots()
+    # fig1.patch.set_facecolor('none')
+    # ax1.set_facecolor('none') 
+    ax1.pie(data, labels=data.index, autopct='%1.1f%%', startangle=90)
+    ax1.axis('equal')
+
+    st.write(''' Number of Data from different Countries''')
+
+    st.pyplot(fig1)
+
+    st.write('''
+             Mean Salary Based On Country
+             ''')
+    data = df.groupby(['Country'])['Salary'].mean().sort_values(ascending=True)
+    st.bar_chart(data)
+
+    st.write('''
+             Mean Salary Based On Experience
+             ''')
     
+    data = df.groupby(['YearsCodePro'])['Salary'].mean().sort_values(ascending=True)
+    st.line_chart(data)
